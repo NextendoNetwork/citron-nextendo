2061df046419e27d91a9a460d703c6b6539683e0
collecting <217662237+CollectingW@users.noreply.github.com>
2026-09-06 23:56:46 -0400

    Bump Mario Kart 8 Deluxe required version to 4.0.0

--- src/common/nextendo_compatible_titles.h
+++ src/common/nextendo_compatible_titles.h
@@ -18,7 +18,7 @@ namespace Nextendo::CompatibleTitles {
 
 inline const std::unordered_map<u64, std::string>& Table() {
     static const std::unordered_map<u64, std::string> table{
-        {0x0100152000022000, "3.0.5"},  // Mario Kart 8 Deluxe
+        {0x0100152000022000, "4.0.0"},  // Mario Kart 8 Deluxe
         {0x01006a800016e000, "13.0.5"}, // Super Smash Bros. Ultimate
         {0x0100f8f0000a2000, "5.5.2"},  // Splatoon 2 (EU)
         {0x01003bc0000a0000, "5.5.2"},  // Splatoon 2 (US)
